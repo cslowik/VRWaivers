08b5688fbba6aa5884b044de02c56dfb4daf0493
Chris Slowik <chris.slowik@gmail.com>
2016-07-14 16:06:30 -0700

    added swiftyjson

--- Podfile
+++ Podfile
@@ -3,7 +3,7 @@ use_frameworks!
 target 'VRWaivers' do
 
     pod 'SnapKit', '~> 0.15.0'
-    
+    pod 'SwiftyJSON'
 end
 
 target 'VRWaiversTests' do
--- Pods/Target Support Files/Pods-VRWaivers/Pods-VRWaivers-frameworks.sh
+++ Pods/Target Support Files/Pods-VRWaivers/Pods-VRWaivers-frameworks.sh
@@ -85,7 +85,9 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "Pods-VRWaivers/SnapKit.framework"
+  install_framework "Pods-VRWaivers/SwiftyJSON.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "Pods-VRWaivers/SnapKit.framework"
+  install_framework "Pods-VRWaivers/SwiftyJSON.framework"
 fi
